99184b37c896da9fd57a59885fd40fa3df9bd019
Donizeth <donizethf8@gmail.com>
2024-10-10 16:39:59 +0000

    alteracao nova

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class MainScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Tela Principal'),
+        title: Text('Home'),
         backgroundColor: Colors.blue,
       ),
       drawer: Drawer(
